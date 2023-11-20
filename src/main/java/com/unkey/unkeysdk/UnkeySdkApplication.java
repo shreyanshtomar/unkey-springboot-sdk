c1ab014f8e9ceae6bc3ffae1d18402832af7d2e0
Shreyansh Tomar <shreyanshtomar99@gmail.com>
2023-11-20 11:25:26 +0530

    fix: converted to library and fixed gradle publish issues

--- src/main/java/com/unkey/unkeysdk/UnkeySdkApplication.java
+++ src/main/java/com/unkey/unkeysdk/UnkeySdkApplication.java
@@ -1,13 +1,13 @@
-package com.unkey.unkeysdk;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class UnkeySdkApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UnkeySdkApplication.class, args);
-	}
-
-}
+//package com.unkey.unkeysdk;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class UnkeySdkApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(UnkeySdkApplication.class, args);
+//	}
+//
+//}
